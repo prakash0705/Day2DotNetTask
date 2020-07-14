e8971163498eee8037fdf1aebb358c9d9d74aa04
Kumaran <Prakash.Kumaran@Chubb.com>
2020-07-14 17:26:31 +0530

    first commit

---
+++ Utility/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter string");
+            string input = Console.ReadLine();
+            Console.WriteLine("Enter the position of word");
+            int position = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the char to parse");
+            char charToParse = Console.ReadLine()[0];
+            Console.WriteLine(LastWord(position, input, charToParse));
+            string[] palindromeArray = { "madam","refer","level","radar","welcome","world","hello"};
+            Program program = new Program();
+            //find Palindrome in array of strings
+            List<string> list=program.GetPalindromes(palindromeArray);
+            if(list.Count==0)
+            {
+                Console.WriteLine($"There is no palindrome in a array of strings" + "[{0}]", string.Join(",", palindromeArray).Trim(','));
+            }
+            else
+            {
+                Console.WriteLine($"Palindromes in array of strings"+"[{0}]",string.Join(",",palindromeArray).Trim(','));
+                foreach (string value in list)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+            //temperature
+            Console.WriteLine("Please select the converter");
+            Console.WriteLine("1 -  From celcius to fahrenheit");
+            Console.WriteLine("2 - From fahrenheit to celcius");
+            string options = Console.ReadLine();
+            double celcius, fahrenheit;
+            switch(options)
+            {
+                case "1":
+                    Console.Write("Please enter celcius temperature: ");
+                    fahrenheit = TemperatureConverter.CelciusToFahrenheit(Console.ReadLine());
+                    Console.WriteLine("Temperature in Fahrenheit is "+ string.Format("{0:0.00}",fahrenheit));
+                    break;
+                case "2":
+                    Console.Write("Please enter fahrenheit temperature: ");
+                    celcius = TemperatureConverter.FahrenheitToCelcius(Console.ReadLine());
+                    Console.WriteLine("Temperature in Celcius is "+ string.Format("{0:0.00}",celcius));
+                    break;
+                default:
+                    Console.WriteLine("Please select converter");
+                    break;
+            }
+
+        }
+        static string LastWord(int position,string input,char charToParse)
+        {
+            string[] array = input.Split(charToParse);
+            for(int i=0;i<array.Length;i++)
+            {
+                if(i==position)
+                {
+                    return $"the word {array[i]} is in position "+position;
+                }
+            }
+            return "There is no word in the position" + position;
+        }
+        List<string> GetPalindromes(string[] array)
+        {
+            List<string> list = new List<string>();
+            string reverse;
+            string element;
+            foreach(string value in array)
+            {
+                element = value;
+                reverse = "";
+                for(int i=element.Length-1;i>=0;i--)
+                {
+                    reverse += element[i];
+                }
+                if(reverse==element)
+                {
+                    list.Add(element);
+                }
+            }
+            return list;
+        }
+
+    }
+}
---
+++ Utility/TemperatureConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility
+{
+    static class TemperatureConverter
+    {
+        public static double CelciusToFahrenheit(string temperatureCelcius)
+        {
+            double celcius = Double.Parse(temperatureCelcius);
+
+            return (celcius * 1.8) + 32;
+           
+        }
+        public static double FahrenheitToCelcius(string temeratureFahrenheit)
+        {
+            double fahrenheit = Double.Parse(temeratureFahrenheit);
+            return ((fahrenheit - 32) * 5) / 9;
+            
+        }
+    }
+}
